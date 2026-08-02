eb5ffe93c67cd510fd16d58100a666e6f4b23911
daisy7942 <zotazoang@gmail.com>
2026-08-02 20:22:20 +0900

    refactor: EapprovalVO 필드를 private/camelCase로 정리하고 getter/setter 추가

--- src/main/java/com/eapproval/employee/vo/EapprovalVO.java
+++ src/main/java/com/eapproval/employee/vo/EapprovalVO.java
@@ -2,9 +2,40 @@ package com.eapproval.employee.vo;
 
 public class EapprovalVO {
 
-	int employeeId;
-	String employee_code;
-    String name;
-    String email;
+	private int employeeId;
+	private String employeeCode;
+	private String name;
+    private String email;
+    
+    
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public String getEmployeeCode() {
+		return employeeCode;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	
+	
+	
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public void setEmployeeCode(String employeeCode) {
+		this.employeeCode = employeeCode;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
+
+
