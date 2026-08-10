5b7760f5a852bae148b6b6981dcd34f3e6ded67f
suhyeon <zotazoang@gmail.com>
2026-08-10 14:06:00 +0900

    feat: 조직도 조회에 manager_id 추가 및 팀장 우선 정렬

--- src/main/java/com/eapproval/employee/vo/OrgVO.java
+++ src/main/java/com/eapproval/employee/vo/OrgVO.java
@@ -1,77 +1,104 @@
 package com.eapproval.employee.vo;
 
 public class OrgVO {
-	private Long departmentId;  
+	private Long departmentId;
 	private String departmentName;
-	private Long teamId;   
+	private Long teamId;
 	private String teamName;
 	private Long employeeId;
 	private String employeeCode;
 	private String name;
-	private String position;  
+	private String position;
 	private Integer positionLevel;
 	private String title;
-	
-	
+	private Long managerId;
+
+	public Long getManagerId() {
+		return managerId;
+	}
+
+	public void setManagerId(Long managerId) {
+		this.managerId = managerId;
+	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public Long getDepartmentId() {
 		return departmentId;
 	}
+
 	public String getDepartmentName() {
 		return departmentName;
 	}
+
 	public Long getTeamId() {
 		return teamId;
 	}
+
 	public String getTeamName() {
 		return teamName;
 	}
+
 	public Long getEmployeeId() {
 		return employeeId;
 	}
+
 	public String getEmployeeCode() {
 		return employeeCode;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public String getPosition() {
 		return position;
 	}
+
 	public Integer getPositionLevel() {
 		return positionLevel;
 	}
+
 	public void setDepartmentId(Long departmentId) {
 		this.departmentId = departmentId;
 	}
+
 	public void setDepartmentName(String departmentName) {
 		this.departmentName = departmentName;
 	}
+
 	public void setTeamId(Long teamId) {
 		this.teamId = teamId;
 	}
+
 	public void setTeamName(String teamName) {
 		this.teamName = teamName;
 	}
+
 	public void setEmployeeId(Long employeeId) {
 		this.employeeId = employeeId;
 	}
+
 	public void setEmployeeCode(String employeeCode) {
 		this.employeeCode = employeeCode;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public void setPosition(String position) {
 		this.position = position;
 	}
+
 	public void setPositionLevel(Integer positionLevel) {
 		this.positionLevel = positionLevel;
 	}
-	
-	}
+
+}
